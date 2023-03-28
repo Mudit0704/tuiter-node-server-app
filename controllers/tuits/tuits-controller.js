79379af7458419a38193af46db73eaf61c211e43
Mudit0704 <maheshwari7mudit@gmail.com>
2023-03-28 14:03:53 -0400

    Adding time support for new tuit.

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -10,6 +10,7 @@ const createTuit = (req, res) => {
   newTuit.disliked = false;
   newTuit.replies = 0;
   newTuit.retuits = 0;
+  newTuit.time = "2h";
   tuits.push(newTuit);
   res.json(newTuit);
 }
