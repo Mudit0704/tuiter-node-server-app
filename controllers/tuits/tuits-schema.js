30b41304db5a1b0ab9cc24e712cd8de857a672a3
Mudit0704 <maheshwari7mudit@gmail.com>
2023-04-10 16:47:28 -0400

    A9 completed.

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,15 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  dislikes : Number,
+  disliked : Boolean,
+  replies : Number,
+  retuits : Number,
+  avatarIcon : String,
+  handle : String,
+  image : String,
+  title : String,
+  topic : String,
+  time : String
 }, {collection: 'tuits'});
 export default schema;
